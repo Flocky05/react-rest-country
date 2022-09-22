f5e6aee7aa9092b87395ed39ad7b128f9293a683
Flocky05 <md.rakibul.islam.0501@gmail.com>
2022-09-22 21:07:35 +0000

    add data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Country from './component/Country/Country';
+import Men from './component/Men/Men';
+import Titel from './component/Title/Titel';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Titel></Titel>
+      <Country></Country>
+      <Men></Men>
     </div>
   );
 }
+// function Countries() {
+//   const [countries, setCountries] = useState([]);
 
+//   useEffect(() => {
+//     fetch("https://restcountries.com/v3.1/all")
+//       .then(res => res.json())
+//       .then(data => setCountries(data))
+//   }
+//     , [])
+//   return (
+//     <div>
+//       {/* <h1>Visisting All countries in the world</h1>
+//       <h2>Total Country :{countries.length}</h2>
+//       {
+//         countries.map(country => <Country Name={country.name.common} Population={country.population}></Country>)
+//       } */}
+//     </div>
+//   )
+// }
+// // function Country(props) {
+// //   return (
+// //     <div className='country'>
+// //       <h3>Country Name:{props.Name}</h3>
+// //       <p>Population:{props.Population}</p>
+// //     </div>
+// //   )
+// // }
 export default App;
---
+++ src/component/Title/Titel.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Title.css';
+const Titel = () => {
+    return (
+        <div>
+            <nav className='menu'>
+                <a href="/Home"> Home</a>
+                <a href="/Shop">Shop</a>
+                <a href="/About us">About us</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Titel;
